6e5a96ee4556654989499cfeed98dca6f53abf09
oeobukhov <112682666+oeobukhov@users.noreply.github.com>
2022-09-07 11:20:39 +0400

    Update task2.py

--- task2/task2.py
+++ task2/task2.py
@@ -1,1 +1,17 @@
+import os 
+import sys
+filename = sys.argv[1]
+with open(filename) as file:
+    nums = file.readlines()
+nums = [int(line.rstrip()) for line in nums]
+n=len(nums)
+min=0
+for j in range(n):
+    min+=abs(nums[0]-nums[j])
+for i in range(1,n-1):
+    sum=0
+    for j in range(n):
+        sum+=abs(nums[i]-nums[j])
+    if sum<min: min=sum
+print(min)
 
